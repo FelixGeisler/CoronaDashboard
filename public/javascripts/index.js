f9991065f685a7c6aa5213a399697e2c0bb4bc75
geisler-3 <felix.geisler@mt.com>
2020-11-14 13:55:38 +0100

    added a footer

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,11 +10,15 @@ let valueArrayBl = [];
 let valueArrayLk = [];
 
 $.getJSON('api/2020-11-14/', function (data) {
+    let flag = true
     $.each(data, function (key, val) {
-        item = document.createElement('li')
-        item.appendChild(document.createTextNode(key));
-        bundeslandDrop.appendChild(item)
-        valueArrayBl.push(key)
+        if (!flag) {
+            item = document.createElement('li')
+            item.appendChild(document.createTextNode(key));
+            bundeslandDrop.appendChild(item)
+            valueArrayBl.push(key) 
+        }
+        flag = false  
     })
 }).done(function (n) {
     let dropdownArrayBl = [...document.querySelectorAll('ul#bundeslandlist.value-list li')];
@@ -45,6 +49,7 @@ $.getJSON('api/2020-11-14/', function (data) {
         const isInputLk = landkreisField.contains(evt.target);
         if (!isDropdownBl && !isInputBl) {
             bundeslandDrop.classList.remove('open');
+            
         }
         if (!isDropdownLk && !isInputLk) {
             landkreisDrop.classList.remove('open');
@@ -66,30 +71,30 @@ $.getJSON('api/2020-11-14/', function (data) {
     dropdownArrayBl.forEach(item => {
         item.addEventListener('click', (evt) => {
             bundeslandField.value = item.textContent;
-            if (bundeslandField.value !== 'All') {
-                $.getJSON('api/2020-11-14/' + item.textContent, function (data) {
-                    $(landkreisDrop).empty()
-                    landkreisField.value = ''
-                    valueArrayLk = []
-                    $.each(data, function (key, val) {
+            landkreisField.value = ''
+            $.getJSON('api/2020-11-14/' + item.textContent, function (data) {
+                $(landkreisDrop).empty()
+                valueArrayLk = []
+                flag = true
+                $.each(data, function (key, val) {
+                    if (!flag) {
                         lk_item = document.createElement('li')
                         lk_item.appendChild(document.createTextNode(key));
                         landkreisDrop.appendChild(lk_item)
-                        valueArrayLk.push(key)
-                    })
-                    dropdownArrayLk = [...document.querySelectorAll('ul#landkreislist.value-list li')]
-                    dropdownArrayLk.forEach(item => {
-                        item.addEventListener('click', (evt) => {
-                            landkreisField.value = item.textContent;
-                            dropdownArrayLk.forEach(landkreisDrop => {
-                                landkreisDrop.classList.add('closed');
-                            });
+                        valueArrayLk.push(key)   
+                    }
+                    flag = false
+                })
+                dropdownArrayLk = [...document.querySelectorAll('ul#landkreislist.value-list li')]
+                dropdownArrayLk.forEach(item => {
+                    item.addEventListener('click', (evt) => {
+                        landkreisField.value = item.textContent;
+                        dropdownArrayLk.forEach(landkreisDrop => {
+                            landkreisDrop.classList.add('closed');
                         });
-                    })
-                })   
-            } else {
-                landkreisField.value = 'All'
-            }
+                    });
+                })
+            })
             dropdownArrayBl.forEach(bundeslandDrop => {
                 bundeslandDrop.classList.add('closed');
             });
