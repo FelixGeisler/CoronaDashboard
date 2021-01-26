875a7da0d05d50afb8a5b77fe806addffaeeea54
geisler-3 <felix.geisler@mt.com>
2021-01-26 02:53:51 +0100

    Add Postman.

--- tests/database.test.js
+++ tests/database.test.js
@@ -1,10 +1,5 @@
-const { ExpectationFailed } = require('http-errors')
-const https = require('https')
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./database/db.sqlite')
-
 test('RKI-API Connection', () => {
-    https.get('https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID,SN_V1,EWZ,cases,deaths&returnGeometry=false&orderByFields=OBJECTID&outSR=&f=json', (resp) => {
+    https.get(sqlString, (resp) => {
         expect(resp.statusCode).toBe(200)
     })
 })
