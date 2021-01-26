042148f979e3530a329b6eb9fadfa356442408b4
geisler-3 <felix.geisler@mt.com>
2021-01-26 11:52:18 +0100

    add ui-test.

---
+++ tests/ui-test.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+async function selectDistrict (stateID, districtID) {
+    browser = await puppeteer.launch({defaultViewport: null});
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    const state = await page.$x(`//*[@id="level2_map_${stateID}"]`)                 // Select State
+    await state[0].click()
+    const district = await page.$x(`//*[@id="level3_map_${districtID}"]`)           // Select District
+    await district[0].click() 
+    await page.screenshot({path: './tests/screenshots/zollernalbkreis.png'});
+}
+selectDistrict(8, 216);
