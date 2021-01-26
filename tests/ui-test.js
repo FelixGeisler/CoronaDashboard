d9fe4e1e6e1798979a231e109c0421e82d239092
geisler-3 <felix.geisler@mt.com>
2021-01-26 12:17:56 +0100

    Fix UI-Test.

--- tests/ui-test.js
+++ tests/ui-test.js
@@ -1,13 +1,14 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 async function selectDistrict (stateID, districtID) {
-    browser = await puppeteer.launch({defaultViewport: null});
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
+    browser = await puppeteer.launch({defaultViewport: null})
+    page = await browser.newPage()
+    await page.goto('http://localhost:3000/')
     const state = await page.$x(`//*[@id="level2_map_${stateID}"]`)                 // Select State
     await state[0].click()
     const district = await page.$x(`//*[@id="level3_map_${districtID}"]`)           // Select District
     await district[0].click() 
-    await page.screenshot({path: './tests/screenshots/zollernalbkreis.png'});
+    await page.screenshot({path: './tests/screenshots/zollernalbkreis.png'})
+    browser.close()
 }
 selectDistrict(8, 216);
